deb58bacf6e802cb1cb4e4de76904c4d265be884
Tcodrz <ttomz4291@gmail.com>
2022-01-29 19:07:12 +0200

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,6 +13,7 @@ import { TeamService } from 'src/app/services/team.service';
 export class LoginComponent implements OnInit {
   @ViewChild('teamName', { static: true }) teamName: ElementRef;
   team: Team;
+  isSubmiting: boolean;
   constructor(
     private activatedRoute: ActivatedRoute,
     private teamService: TeamService,
@@ -21,21 +22,9 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.isSubmiting = false;
     this.activatedRoute.params.subscribe(params => {
       const team = this.cacheService.getItem<Team>(LocalStorageKeys.Team);
-      if (!!team) {
-        const timeout = 60 * 1 * 1000;
-        const now: number = new Date().getTime();
-        const timeFinished = new Date(team.finishedAt).getTime();
-        const timePasssed = now - timeFinished;
-        if (timePasssed >= timeout) {
-          this.cacheService.clear();
-          this.router.navigate([`login/${params.pageID}`]);
-          return;
-        }
-        if (team.finished) this.router.navigate([`results/${team.finalTime}`]);
-        else this.router.navigate([`page/${team.pageID}`]);
-      }
       this.team = {
         pageID: params.pageID,
         id: '',
@@ -43,10 +32,25 @@ export class LoginComponent implements OnInit {
         finished: false,
         started: false,
       }
+      if (!!team) {
+        const cooldownTime = 1; // how much time the user should stay locked out from the game in minutes
+        const cooldownMs = cooldownTime * 60 * 1000; // total cooldown time in milliseconds
+        const now = new Date().getTime();
+        const timeFinished = new Date(team.finishedAt).getTime();
+        const timePasssed = now - timeFinished;
+        if (timePasssed >= cooldownMs) {
+          this.cacheService.clear();
+          this.router.navigate([`login/${params.pageID}`]);
+          return;
+        }
+        if (team.finished) this.router.navigate([`results/${team.finalTime}`]);
+        else this.router.navigate([`page/${team.pageID}`]);
+      }
     });
   }
   onSubmit(teamName: string): void {
     if (!this.validate(teamName)) return;
+    this.isSubmiting = true;
     this.team.name = teamName;
     this.teamService.createTeam(this.team);
   }
