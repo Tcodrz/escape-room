2ae3505dbf017bc9ca2f21cfbe0b5b114233685c
Tcodrz <ttomz4291@gmail.com>
2022-01-29 19:08:39 +0200

    question component messages

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { Question } from 'src/app/interface/question.interface';
-
+type messageTypes = 'warn' | 'success' | 'info';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
@@ -9,10 +9,13 @@ import { Question } from 'src/app/interface/question.interface';
 export class QuestionComponent implements OnInit {
   @Input() question: Question;
   @Input() questionNumber: number;
+  @Input() totalQuestions: number;
   @Output() clueRecieved: EventEmitter<void> = new EventEmitter<void>();
   @Output() answerSuccess: EventEmitter<void> = new EventEmitter<void>();
   @Output() answerFail: EventEmitter<void> = new EventEmitter<void>();
   @ViewChild('answer', { static: false }) answerInput: ElementRef;
+  message: string;
+  messageColor: messageTypes;
   clueIndex = 0;
   clues: string[] = [];
   constructor() { }
@@ -22,19 +25,29 @@ export class QuestionComponent implements OnInit {
       this.clues.push(this.question.hints[this.clueIndex]);
       this.clueRecieved.emit();
       this.clueIndex++;
-      setTimeout(() => {
-        this.answerInput.nativeElement.focus();
-      }, 300);
+      this.showMessage('נוספה דקה לשעון', 'warn');
     } else {
       // show message 'NO MORE CLUES...'
     }
   }
   onSubmit(answer: string): void {
     if (this.question.a === answer) {
-      this.answerSuccess.emit();
+      this.showMessage('תשובה נכונה!', 'success');
+      setTimeout(() => {
+        this.answerSuccess.emit();
+      }, 1000);
     } else {
       this.answerFail.emit();
+      this.showMessage('תשובה לא נכונה, נוספו 2 דקות לשעון', 'warn');
     }
+  }
+  showMessage(message: string, color: messageTypes): void {
+    this.message = message;
+    this.messageColor = color;
+    setTimeout(() => {
+      this.message = undefined;
+    }, 2500);
+
   }
 }
 
--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -4,6 +4,8 @@ import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenuModule } from 'primeng/menu';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
 
 @NgModule({
   declarations: [],
@@ -13,12 +15,16 @@ import { MenuModule } from 'primeng/menu';
     ButtonModule,
     InputTextModule,
     MenuModule,
+    MessagesModule,
+    MessageModule,
   ],
   exports: [
     CardModule,
     ButtonModule,
     InputTextModule,
     MenuModule,
+    MessagesModule,
+    MessageModule,
   ]
 })
 export class PrimengModule { }
