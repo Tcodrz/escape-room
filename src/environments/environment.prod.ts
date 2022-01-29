6d36d802198e28025d5cf398a418f595e176ca69
Tcodrz <ttomz4291@gmail.com>
2022-01-29 17:45:36 +0200

    firebase config production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  useEmulators: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBjhhhgSv6NfbQyxEaWFUVnbAEBQSRvusw",
+    authDomain: "escape-room-a8e85.firebaseapp.com",
+    projectId: "escape-room-a8e85",
+    storageBucket: "escape-room-a8e85.appspot.com",
+    messagingSenderId: "1017771351734",
+    appId: "1:1017771351734:web:c1416a0c3e51b6c4a01017",
+    measurementId: "G-BKWRQ511DS"
+  },
 };
