cd18fe81b7401a979f11763c731cf84dd2347901
Tcodrz <ttomz4291@gmail.com>
2022-01-29 19:07:36 +0200

    dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { MenuItem } from 'primeng/api';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Team } from 'src/app/interface/team.interface';
 
 @Component({
@@ -9,23 +9,27 @@ import { Team } from 'src/app/interface/team.interface';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   teams$: Observable<Team[]>;
   menuItems: MenuItem[];
   teams: Team[];
+  sub: Subscription;
   constructor(
     private db: AngularFirestore,
   ) { }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.teams$ = this.db.collection<Team>('teams').valueChanges();
+    this.sub = this.teams$.subscribe(teams => this.teams = teams)
     this.menuItems = [
       {
         label: 'איפוס משחק',
         icon: 'pi pi-replay',
         command: () => {
-          this.teams$.subscribe(teams => this.teams = teams)
-          this.resetGame()
+          this.resetGame();
         }
       }
     ]
